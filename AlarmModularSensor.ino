9e2aace76f66213730bf720b89cf39300176d060
Robert Hemingway <robert.hemingway@outlook.com>
2025-09-08 20:56:21 +1000

    8Sep25 Initial code entry

---
+++ AlarmModularSensor.ino
@@ -0,0 +1,13 @@
+/*
+ 08Sep2025 : initial code entry
+ V1 will be to get an ESP32 into deep sleep and to wake up by
+    externally triggering an AM312 PIR
+
+    My Onedrive Deep Sleep notes
+    https://onedrive.live.com/view.aspx?resid=AF5CDF486EAF148A%21717409&id=documents&wd=target%28ESP32.one%7C2B67EE99-FA3C-4FA7-834B-1E3FA7F8FABA%2FDeep%20Sleep%20test%7C3DC48E4D-534F-4792-AA5E-2A9F0C9356A5%2F%29&wdpartid={7609B815-4998-00F4-229D-B2E2BFCB85DF}{1}&wdsectionfileid=AF5CDF486EAF148A!717751
+
+    My Joplin note
+    https://joplin://x-callback-url/openNote?id=50c24b80035e46bd9e58b46a55180681
+
+    
+ */
